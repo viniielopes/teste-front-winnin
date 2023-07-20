d24a287da9f9d97dbdd9464efa56408868a6e165
Vinicius Lopes <viniielopes@live.com>
2023-07-20 19:37:51 -0300

    refactor: removed paddingX and set button width

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ export const Button = ({ active = false, children }: ButtonProps) => {
 
   return (
     <button
-      className={`last:mr-0 mr-4 rounded-lg ${bg} text-xl leading-6 py-3 px-16 text-white font-semibold`}
+      className={`last:mr-0 mr-4 rounded-lg ${bg} text-xl leading-6 py-3 w-48 text-white font-semibold`}
     >
       {children}
     </button>
