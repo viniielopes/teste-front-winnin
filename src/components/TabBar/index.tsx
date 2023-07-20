8c1dc2316312cb3bdf8de0542f653af5441b4841
Vinicius Lopes <viniielopes@live.com>
2023-07-20 19:38:04 -0300

    refactor: removed last selector

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -2,7 +2,7 @@ import { Button } from "../Button";
 
 export const TabBar = () => {
   return (
-    <div className="flex justify-center mt-5 last:mr-0 last-of-type:mr-0 ">
+    <div className="flex justify-center mt-5">
       <Button active>Hot</Button>
       <Button>News</Button>
       <Button>Rising</Button>
