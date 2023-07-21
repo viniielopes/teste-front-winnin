2a25f6d735d73174e92b1280e361f39fe7c64f2b
Vinicius Lopes <viniielopes@live.com>
2023-07-20 23:03:26 -0300

    feat: added two types of width and onclick in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,29 @@
-import { ButtonProps } from "./types";
+"use client";
+import { useCallback } from "react";
+import { ButtonProps, ButtonWidthType } from "./types";
 
-export const Button = ({ active = false, children }: ButtonProps) => {
+export const Button = ({
+  active = true,
+  children,
+  width = "normal",
+  onClick = () => {},
+}: ButtonProps) => {
+  const widthSize: { [key in ButtonWidthType]: string } = {
+    normal: "w-48",
+    full: "w-full",
+  };
+
   const bg = active ? "bg-primary" : "bg-gray";
+  const btnWidth = widthSize[width];
+
+  const onClickButton = useCallback(() => {
+    onClick(children);
+  }, []);
 
   return (
     <button
-      className={`last:mr-0 mr-4 rounded-lg ${bg} text-xl leading-6 py-3 w-48 text-white font-semibold`}
+      className={`${bg} ${btnWidth} hover:bg-active-primary active:bg-active-primary last:mr-0 mr-4 rounded-lg text-xl leading-6 py-3 w-48 text-white font-semibold`}
+      onClick={onClickButton}
     >
       {children}
     </button>
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,8 @@
+export type ButtonWidthType = "normal" | "full";
+
 export type ButtonProps = {
   active?: boolean;
   children: string;
+  onClick?(text: string): void;
+  width?: ButtonWidthType;
 };
