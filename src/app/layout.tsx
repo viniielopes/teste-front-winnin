6e76a0b811e1b4b08172036cc3f85308cb8edae5
Vinicius Lopes <viniielopes@live.com>
2023-07-20 23:02:44 -0300

    refactor: move header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="w-screen bg-primary flex justify-center">
+          <h1 className="text-4xl font-bold py-6 text-white">
+            REACT<span className="text-feedback-warning">JS</span>
+          </h1>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
